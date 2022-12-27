4b35101bf3587e6720da7eb0944cd61245073469
adhav <uniqname@umich.edu>
2022-12-27 14:46:58 -0500

    Created basic files for event system

---
+++ Assets/CLAWS/Vitals/Vitals.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Vitals
+{
+    public double O2;
+    public double CO2;
+    public double WaterPressure;
+    public double SuitPressure;
+}
