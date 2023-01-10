8fda741d1c71e0c07bd377a1a80509933dd5b192
btschneid <btschneid3@gmail.com>
2023-01-09 22:49:13 -0500

    updated script
    script updated

--- Assets/CLAWS/Menu/scripts/MenuVital.cs
+++ Assets/CLAWS/Menu/scripts/MenuVital.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
+​
 public class MenuVital : MonoBehaviour
 {
+    [SerializeField] TextMeshProUGUI vitalText1;
+    [SerializeField] TextMeshProUGUI vitalText2;
+    [SerializeField] TextMeshProUGUI vitalText3;
+    private Vitals script;
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
+​
     // Update is called once per frame
     void Update()
     {
-        
+        vitalText1.SetText(script.O2.ToString());
+        vitalText2.SetText("Battery: 84%");
+        vitalText3.SetText(script.WaterPressure.ToString());
     }
 }
