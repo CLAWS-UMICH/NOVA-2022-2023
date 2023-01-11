bfecba6cdf1b96e7b433b8fa780ec1abbc880a95
btschneid <btschneid3@gmail.com>
2023-01-11 13:29:13 -0500

    Added vitals text
    with the script

--- Assets/CLAWS/Menu/scripts/MenuVital.cs
+++ Assets/CLAWS/Menu/scripts/MenuVital.cs
@@ -5,21 +5,16 @@ using TMPro;
 
 public class MenuVital : MonoBehaviour
 {
-    [SerializeField] TextMeshProUGUI vitalText1;
-    [SerializeField] TextMeshProUGUI vitalText2;
-    [SerializeField] TextMeshProUGUI vitalText3;
+    [SerializeField] TextMeshPro vitalText1;
+    [SerializeField] TextMeshPro vitalText2;
+    [SerializeField] TextMeshPro vitalText3;
     private Vitals script;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        vitalText1.SetText(script.O2.ToString());
-        vitalText2.SetText("Battery: 84%");
-        vitalText3.SetText(script.WaterPressure.ToString());
+        //vitalText1.SetText(script.O2.ToString());
+        //vitalText2.SetText("84%");
+        //vitalText3.SetText(script.WaterPressure.ToString());
     }
 }
