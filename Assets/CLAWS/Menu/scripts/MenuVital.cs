496d3458bbb1d17b7cd3e1385570666380e1441c
selinal <selnal@umich.edu>
2023-01-11 20:14:21 -0500

    yay we did it..

--- Assets/CLAWS/Menu/scripts/MenuVital.cs
+++ Assets/CLAWS/Menu/scripts/MenuVital.cs
@@ -8,13 +8,12 @@ public class MenuVital : MonoBehaviour
     [SerializeField] TextMeshPro vitalText1;
     [SerializeField] TextMeshPro vitalText2;
     [SerializeField] TextMeshPro vitalText3;
-    private Vitals script;
 
     // Update is called once per frame
     void Update()
     {
-        //vitalText1.SetText(script.O2.ToString());
-        //vitalText2.SetText("84%");
-        //vitalText3.SetText(script.WaterPressure.ToString());
+        // vitalText1.SetText((Simulation.User.AstronautVitals.O2).ToString());
+        vitalText2.SetText("84%");
+        vitalText3.SetText((Simulation.User.AstronautVitals.SuitPressure).ToString());
     }
 }
