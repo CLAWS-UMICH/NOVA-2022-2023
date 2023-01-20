9ebd5b4abd83beff3e5facc89952498ff025db07
Kriti Gupta <kritigupta@gmail.com>
2023-01-19 20:12:43 -0500

    warning color

---
+++ Assets/WarningVitals.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WarningVitals : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void VitalLevel(){
+        Vitals TempVital = Simulation.User.AstronautVitals;
+        //Debug.Log("hi1");
+        if(TempVital.O2 >= 11){
+            // var colorTheme = this.GetComponent<Interactable>().ActiveThemes[0];
+            // colorTheme.StateProperties[0].Values[0].Color = Color.green;
+            //Debug.Log("hi");
+            gameObject.GetComponent<MeshRenderer>().material.color = Color.red;
+        }
+        // if(TempVital.CO2 >= 12){
+            
+        // }
+        // if(TempVital.SuitPressure >= 12){
+            
+        // }
+        // if(TempVital.WaterPressure >= 12){
+            
+        // }
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        VitalLevel();
+    }
+}
