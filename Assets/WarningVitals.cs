6c6dbedc31c4e79b12c4afb2a1dd69ecc4e9d94d
Kriti Gupta <kritigupta@gmail.com>
2023-01-21 16:14:43 -0500

    red banner

--- Assets/WarningVitals.cs
+++ Assets/WarningVitals.cs
@@ -1,24 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
+//using System.Windows.Media;
 using UnityEngine;
 using UnityEngine.UI;
+
+using TMPro;
 
 public class WarningVitals : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    [SerializeField] GameObject panel;
+    [SerializeField] TextMeshPro warningText;
+    [SerializeField] GameObject largePanel;
+    [SerializeField] Color red;
     void Start()
     {
-
+        EventBus.Subscribe<VitalsUpdatedEvent>(VitalLevel);
     }
 
-    void VitalLevel(){
+    void VitalLevel(VitalsUpdatedEvent e){
         Vitals TempVital = Simulation.User.AstronautVitals;
         //Debug.Log("hi1");
-        if(TempVital.p_o2 >= 11){
+        if(TempVital.p_o2 >= 3){
             // var colorTheme = this.GetComponent<Interactable>().ActiveThemes[0];
             // colorTheme.StateProperties[0].Values[0].Color = Color.green;
             //Debug.Log("hi");
-            gameObject.GetComponent<MeshRenderer>().material.color = Color.red;
+            // gameObject.GetComponent<MeshRenderer>().material.color = Color.red;
+            //Color color = (Color)ColorConverter.ConvertFromString("#FFDFD991");
+            gameObject.GetComponent<MeshRenderer>().material.color = red;
+            //Color color = (Color)ColorConverter.ConvertFromString("#FFDFD991");
+            //new Color(0.4f, 0.9f, 0.7f, 1.0f);
+            if(!largePanel.activeSelf){
+                panel.GetComponent<MeshRenderer>().material.color = red;
+                warningText.SetText("O2 levels low");
+                panel.SetActive(true);
+                
+                
+                // panel.transform.GetChild().GetComponent<TextMeshProUGUI>.text = "O2 levels";
+                
+
+            }
+            
         }
         // if(TempVital.CO2 >= 12){
             
@@ -36,6 +59,8 @@ public class WarningVitals : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        VitalLevel();
+        if(gameObject.GetComponent<MeshRenderer>().enabled == false){
+            gameObject.GetComponent<MeshRenderer>().enabled = true;
+        }
     }
 }
