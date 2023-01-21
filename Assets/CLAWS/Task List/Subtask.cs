a1bc9f0ad9aa15d04051eb1663125d68673e3ee3
adhav <uniqname@umich.edu>
2023-01-21 15:55:34 -0500

    finished tasklist scrolling scripts

--- Assets/CLAWS/Task List/Subtask.cs
+++ Assets/CLAWS/Task List/Subtask.cs
@@ -5,5 +5,18 @@ using TMPro;
 
 public class Subtask
 {
-    public TMP_Text SubtaskText;
+    public string title;
+    public string description;
+    public char taskType;
+
+    public Subtask() {
+        title = null;
+        description = null;
+        taskType = '\0';
+    }
+    public Subtask(string title_in, string desc_in, char type_in) {
+        title = title_in;
+        description = desc_in;
+        taskType = type_in;
+    }
 }
