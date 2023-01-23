29ce98041c7aa61744873615359825ca61a8804f
Samuel David Bohnett <onefrfry@umich.edu>
2023-01-23 10:10:37 -0500

    Added PanelInformation

---
+++ Assets/CLAWS/Vitals/BohnettNOVA/Bohnett/Vitals/Scripts/PanelInformation.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelInformation : MonoBehaviour
+{
+    public enum InformationType {OxygenPrimary, OxygenBackup, SuitPressure, WaterPressure,
+                                    BatteryBackup, Fan, Water}
+}
