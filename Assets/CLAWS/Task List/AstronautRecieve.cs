91ac7777d8f672ecdb7ee0550cd5279f545b4ad0
jasonf12369 <jjfu@umich.edu>
2023-01-25 16:48:17 -0500

    connecte websocekt to front

--- Assets/CLAWS/Task List/AstronautRecieve.cs
+++ Assets/CLAWS/Task List/AstronautRecieve.cs
@@ -23,9 +23,6 @@ public class RecieveMessage : WebSocketBehavior
 }
 public class AstronautRecieve : MonoBehaviour
 {
-    private TcpListener server;
-    public TMP_Text text;
-
     public string address;
     public int port;
 
@@ -45,14 +42,7 @@ public class AstronautRecieve : MonoBehaviour
         while (Simulation.User.AstronautTasks.messageQueue.TryDequeue(out string message))
         {
             JsonMessage readIn = JsonConvert.DeserializeObject<JsonMessage>(message);
-            //var msgJson = JObject.Parse(@message);
-            //string messageType = (string)msgJson["message_type"];
             HandleMessage(readIn.message_type, message);
-            //text.SetText(
-            //    "Task Id: " + readIn.task_list[0].taskId+
-            //    "\nTask Title: " + readIn.task_list[0].taskTitle +
-            //    "\nTask Desc: " + readIn.task_list[0].taskDesc
-            //);
         }
     }
 
@@ -65,53 +55,8 @@ public class AstronautRecieve : MonoBehaviour
                 //Simulation.User.AstronautTasks.taskList
                 Debug.Log("updatedTaskList");
                 TaskListUpdated readIn = JsonConvert.DeserializeObject<TaskListUpdated>(message);
-                //TODO: Set values recieved from websocket to Simulation.User Astronaut class. 
-                //Simulation.User.AstronautTasks.tasksUpdated(msgJson);
-                //EventBus.Publish<TasksUpdatedEvent>(new TasksUpdatedEvent());
-                text.SetText(
-                    "Task Id: " + readIn.task_list[0].taskId +
-                    "\nTask Title: " + readIn.task_list[0].taskTitle +
-                    "\nTask Desc: " + readIn.task_list[0].taskDesc
-                );
+                Simulation.User.AstronautTasks.tasksUpdated(readIn.task_list);
                 break;
         }
     }
-
-    //Code below uses sockets and listens manually
-    //private void RecieveFromMCC() { 		
-    //    try {
-    //        // Create listener on <address>:<port>.
-    //        server = new TcpListener(IPAddress.Parse(address), port);
-    //        server.Start();
-    //        Debug.Log("WebSocket server is listening for incoming connections.");
-
-    //        Byte[] bytes = new Byte[256];
-    //        String message = null;
-
-    //        while (true) {
-    //            // Accept a new client, then open a stream for reading and writing.
-    //            Debug.Log("WebSocket server is waiting for clients");
-    //            TcpClient client = server.AcceptTcpClient();
-    //            // Get a stream object for reading and writing
-    //            NetworkStream stream = client.GetStream();
-    //            // Loop to receive all the data sent by the client.
-    //            int i;
-    //            while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
-    //            {
-    //                string addition = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
-    //                Console.WriteLine("Received: {0}", addition);
-    //                message += addition;
-    //            }
-    //            //Convert string into json object
-    //            //var messageJson = JObject.Parse(message);
-    //            //Handle message
-    //            //string messageType = (string)messageJson["message_type"];
-    //            HandleMessage(message);
-    //            Debug.Log("afterhandle");
-    //        }
-    //    }
-    //    catch (SocketException socketException) {
-    //        Debug.Log("SocketException " + socketException.ToString());
-    //    }
-    //}
 }
