eafcc2ebd3eb4c949075b97cec2c4ae2fbc1008b
Patrick Halim <pnhalim@umich.edu>
2023-02-04 23:16:20 -0500

    fix button close

---
+++ Assets/ScreenManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenManager : MonoBehaviour
+{
+
+    public void CloseScreen(GameObject Screen)
+    {
+        StartCoroutine(_CloseScreen(Screen));
+    }
+    IEnumerator _CloseScreen(GameObject Screen)
+    {
+        yield return new WaitForSeconds(1f);
+        Screen.SetActive(false);
+    }
+}
