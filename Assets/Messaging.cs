3bdeeffcb6ecc71bfe1530ec64ec6a48a60478f0
Joel <joelyson@DESKTOP-VLF4QBF.localdomain>
2023-02-15 20:15:18 -0500

    Add new text scene

---
+++ Assets/Messaging.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Messaging: MonoBehaviour
+{
+    public class MessageFamily
+    {
+        class MessagingHandler
+        {
+            //Current char object
+            //Chat id lookup
+        }
+        class Message
+        {
+            //timestamp
+            //sender
+            //text
+        }
+        class Chat
+        {
+            //astronauts
+            //array of messages
+        }
+    }
+}
+   
