50ecd4ec6083dea692cfd52d13b2fa0523b0e48f
jasonf12369 <jjfu@umich.edu>
2023-02-16 19:05:34 -0500

    messaging test

---
+++ Assets/CLAWS/Messaging/MessageClasses.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+class Message
+{
+    string text;
+    string sender;
+}
+class Chat
+{
+    List<string> members;
+    List<string> messages;
+}
+
+class MessageJson
+{
+    public string message_type { get; set; }
+    public string recipient { get; set; }
+    public string content { get; set; }
+}
---
+++ Assets/CLAWS/Messaging/MessagingHandler.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using UnityEngine;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+using TMPro;
+
+
+public class MessageHandler: MonoBehaviour
+{
+    public TextMeshPro window;
+    public string address;
+    public int port;
+    private WebSocket connection;
+    public ConcurrentQueue<string> messageQueue;
+
+    private void Start()
+    {
+        address = "127.0.0.1";
+        port = 6969;
+        //string url = "ws://" + address + ":" + port;
+        string url1 = "ws://127.0.0.1:6969";
+        connection = new WebSocket(url1);
+        // Set behavior for this websocket when message is recieved
+        connection.OnMessage += (sender, e) =>
+        {
+            OnMessage(e);
+            Debug.Log("MCC message: " + e.Data);
+        };
+        connection.Connect();
+        connection.Send("hellobitch");
+        Debug.Log("Connected to server");
+    }
+
+    // Continously checks if a new message has been recieved
+    private void Update()
+    {
+        while (Simulation.User.AstronautMessaging.messageQueue.TryDequeue(out string message))
+        {
+            MessageJson readIn = JsonConvert.DeserializeObject<MessageJson>(message);
+            HandleMessage(readIn.message_type, message);
+        }
+    }
+
+    private void SendMCC(int messageTemplate)
+    {
+        string recipient = "joel";
+        string testmsg = "shitsfucked";
+        switch (messageTemplate)
+        {
+            case 0:
+                testmsg = "hello";
+                break;
+            case 1:
+                testmsg = "thankyou";
+                break;
+            case 2:
+                testmsg = "bye";
+                break;
+        }
+        string message = "{\"message_type\":\"dm\",\"recipient\":" + recipient + ",\"task_id\":" + testmsg + "}";
+        connection.Send(message);
+        Debug.Log("Sent: " + message);
+    }
+
+    private void OnMessage(MessageEventArgs e)
+    {
+        Debug.Log("Inside WebsocektBehavior");
+        Debug.Log(e.Data);
+        Simulation.User.AstronautMessaging.messageQueue.Enqueue(e.Data);
+    }
+
+    private void HandleMessage(string messageType, string message)
+    {
+        switch (messageType)
+        {
+            //Determine if the task is going to be appended or inserted
+            case "dm":
+                //Simulation.User.AstronautTasks.taskList
+                Debug.Log("recieved message");
+                MessageJson readin = JsonConvert.DeserializeObject<MessageJson>(message);
+                //update text with message
+                break;
+        }
+    }
+
+}
