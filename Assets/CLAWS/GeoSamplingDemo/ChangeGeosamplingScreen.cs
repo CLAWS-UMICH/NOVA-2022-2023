1d7cbc6a235bc03bcfc8470373b375b4052cf11b
Derek Yang <dereky@umich.edu>
2023-04-08 15:45:06 -0400

    Add different layouts for geosampling screens

--- Assets/CLAWS/GeoSamplingDemo/ChangeGeosamplingScreen.cs
+++ Assets/CLAWS/GeoSamplingDemo/ChangeGeosamplingScreen.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class ChangeGeosamplingScreen : MonoBehaviour
 {
+    [SerializeField] private GameObject taskMenu1;
+    [SerializeField] private GameObject taskMenu2;
+    [SerializeField] private GameObject sampleMenu1;
+    [SerializeField] private GameObject sampleMenu2;
+    [SerializeField] private GameObject largeSampleMenu;
+    [SerializeField] private GameObject cameraMenu;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +22,55 @@ public class ChangeGeosamplingScreen : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void ClickButton1()
+    {
+        taskMenu1.SetActive(true);
+        taskMenu2.SetActive(false);
+        sampleMenu1.SetActive(true);
+        sampleMenu2.SetActive(false);
+        largeSampleMenu.SetActive(false);
+        cameraMenu.SetActive(false);
+    }
+
+    public void ClickButton2()
+    {
+        taskMenu1.SetActive(false);
+        taskMenu2.SetActive(true);
+        sampleMenu1.SetActive(false);
+        sampleMenu2.SetActive(true);
+        largeSampleMenu.SetActive(false);
+        cameraMenu.SetActive(false);
+    }
+
+    public void ClickButton3()
+    {
+        taskMenu1.SetActive(false);
+        taskMenu2.SetActive(false);
+        sampleMenu1.SetActive(true);
+        sampleMenu2.SetActive(false);
+        largeSampleMenu.SetActive(false);
+        cameraMenu.SetActive(false);
+    }
+
+    public void ClickButton4()
+    {
+        taskMenu1.SetActive(false);
+        taskMenu2.SetActive(false);
+        sampleMenu1.SetActive(false);
+        sampleMenu2.SetActive(false);
+        largeSampleMenu.SetActive(true);
+        cameraMenu.SetActive(false);
+    }
+
+    public void ClickButton5()
+    {
+        taskMenu1.SetActive(false);
+        taskMenu2.SetActive(false);
+        sampleMenu1.SetActive(false);
+        sampleMenu2.SetActive(false);
+        largeSampleMenu.SetActive(false);
+        cameraMenu.SetActive(true);
     }
 }
