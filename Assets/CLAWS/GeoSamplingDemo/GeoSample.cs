1adbacfe6b355e0d6ad4165a85d4f22cfab0b45a
adhav <uniqname@umich.edu>
2023-05-13 01:32:30 -0400

    Consolidated Geosample

--- Assets/CLAWS/GeoSamplingDemo/GeoSample.cs
+++ Assets/CLAWS/GeoSamplingDemo/GeoSample.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 //Geosample 
+[System.Serializable]
 public class GeoSample 
 {
     public int sampleID;
@@ -13,4 +14,23 @@ public class GeoSample
     public string RFID;
     public char taskType;
     public string description;
+    public GeoSample() {
+        sampleID = -1;
+        rockType = "";
+        lunarTime = "";
+        location = "";
+        RFID = "";
+        taskType = '\0';
+        description = "";
+    }
+    public GeoSample(int sampleID_in, string rockType_in, string lunarTime_in,
+    string location_in, string RFID_in, char taskType_in, string description_in) {
+        sampleID = sampleID_in;
+        rockType = rockType_in;
+        lunarTime = lunarTime_in;
+        location = location_in;
+        RFID = RFID_in;
+        taskType = taskType_in;
+        description = description_in;
+    }
 }
