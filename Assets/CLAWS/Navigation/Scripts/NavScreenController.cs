dc55d8250f46cbc9c6ee25300d6a8118bb3d702f
btschneid <btschneid3@gmail.com>
2023-05-13 16:52:08 -0400

    Created Icons For Navigation Map
    Changed Map for navigatio

--- Assets/CLAWS/Navigation/Scripts/NavScreenController.cs
+++ Assets/CLAWS/Navigation/Scripts/NavScreenController.cs
@@ -24,11 +24,19 @@ public class NavScreenController : MonoBehaviour
     [SerializeField] GameObject obstacleTag;
     [SerializeField] GameObject UIWaypoint;
 
+    // Predetermed Objects
     [SerializeField] GameObject landerObject;
     [SerializeField] GameObject crewObject;
+    [SerializeField] GameObject geoObject;
+    [SerializeField] GameObject mission1Object;
+    [SerializeField] GameObject mission2Object;
+    [SerializeField] GameObject mission3Object;
 
     string globalWaypointTextTitle;
     string globalWaypointType;
+    string globalLat;
+    string globalLong;
+    string currentScreenOpen;
 
     Transform previousEndGoal = null;
 
@@ -48,7 +56,9 @@ public class NavScreenController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        createPreDeterminedPoints();
         currentEndPosition = null;
+        currentScreenOpen = "";
         openNavMenuButton.SetActive(true);
         mainNavScreen.SetActive(false);
         crewScreen.SetActive(false);
@@ -63,6 +73,7 @@ public class NavScreenController : MonoBehaviour
 
     public void CloseAll()
     {
+        currentScreenOpen = "";
         currentEndPosition = null;
         StartCoroutine(_CloseScreen());
         SetAllCullingToCamera();
@@ -96,6 +107,7 @@ public class NavScreenController : MonoBehaviour
 
     public void OpenNavMainMenu()
     {
+        currentScreenOpen = "Menu";
         StartCoroutine(_OpenNavMainMenu());
     }
 
@@ -114,6 +126,7 @@ public class NavScreenController : MonoBehaviour
 
     public void OpenCrewScreen()
     {
+        currentScreenOpen = "Crew";
         StartCoroutine(_OpenCrewScreen());
     }
 
@@ -132,6 +145,7 @@ public class NavScreenController : MonoBehaviour
 
     public void OpenGeoScreen()
     {
+        currentScreenOpen = "Geo";
         StartCoroutine(_OpenGeoScreen());
     }
 
@@ -150,6 +164,7 @@ public class NavScreenController : MonoBehaviour
 
     public void OpenMissionScreen()
     {
+        currentScreenOpen = "Mission";
         StartCoroutine(_OpenMissionScreen());
     }
 
@@ -168,6 +183,7 @@ public class NavScreenController : MonoBehaviour
 
     public void OpenRoverScreen()
     {
+        currentScreenOpen = "Rover";
         StartCoroutine(_OpenRoverScreen());
     }
 
@@ -186,6 +202,7 @@ public class NavScreenController : MonoBehaviour
 
     public void OpenLanderScreen()
     {
+        currentScreenOpen = "Lander";
         StartCoroutine(_OpenLanderScreen());
     }
 
@@ -242,7 +259,11 @@ public class NavScreenController : MonoBehaviour
 
     void ShowOnlyRoverIcons()
     {
-        SetAllCullingToCamera();
+        mapCam.cullingMask |= 1 << LayerMask.NameToLayer("MissionLayer");
+        mapCam.cullingMask |= 1 << LayerMask.NameToLayer("GeoLayer");
+        mapCam.cullingMask |= 1 << LayerMask.NameToLayer("LanderLayer");
+        mapCam.cullingMask |= 1 << LayerMask.NameToLayer("RoverLayer");
+        mapCam.cullingMask &= ~(1 << LayerMask.NameToLayer("CrewLayer"));
     }
 
 
@@ -255,48 +276,88 @@ public class NavScreenController : MonoBehaviour
     // WAYPOINTS
     // FOR TESTING ONLY SO YOU CAN SEE IF THE CONFIRMATION SCREEN OPENS USE THIS FUNCTIOn:
 
-    List<GameObject> missionList = new List<GameObject>();
-    List<GameObject> geoList = new List<GameObject>();
-    List<GameObject> roverList = new List<GameObject>();
+    List<Waypoint> missionList = new List<Waypoint>();
+    List<Waypoint> geoList = new List<Waypoint>();
+    List<Waypoint> roverList = new List<Waypoint>();
+    List<Waypoint> crewList = new List<Waypoint>();
     public List<Waypoint> allWaypoints = new List<Waypoint>();
+
+    private void createPreDeterminedPoints()
+    {
+
+        Waypoint newCrew = new Waypoint(crewObject.transform, "Patrick", (Type)System.Enum.Parse(typeof(Type), "crew"));
+        crewObject.transform.Find("Icons/Letter/LetterText").GetComponent<TextMeshPro>().text = newCrew.GetLetter();
+        crewList.Add(newCrew);
+        allWaypoints.Add(newCrew);
+
+        Waypoint newGeo = new Waypoint(geoObject.transform, "MRS-001", (Type)System.Enum.Parse(typeof(Type), "geosample"));
+        geoObject.transform.Find("Icons/Letter/LetterText").GetComponent<TextMeshPro>().text = newGeo.GetLetter();
+        createGeoButton(newGeo.GetTitle(), newGeo.GetLetter());
+        createRoverButtons(newGeo.GetTitle(), newGeo.GetLetter());
+        geoList.Add(newGeo);
+        roverList.Add(newGeo);
+        allWaypoints.Add(newGeo);
+
+        Waypoint newMission1 = new Waypoint(mission1Object.transform, "Lunar Hiking", (Type)System.Enum.Parse(typeof(Type), "regular"));
+        mission1Object.transform.Find("Icons/Letter/LetterText").GetComponent<TextMeshPro>().text = newMission1.GetLetter();
+        createRegularButton(newMission1.GetTitle(), newMission1.GetLetter());
+        createRoverButtons(newMission1.GetTitle(), newMission1.GetLetter());
+        missionList.Add(newMission1);
+        roverList.Add(newMission1);
+        allWaypoints.Add(newMission1);
+
+        Waypoint newMission2 = new Waypoint(mission2Object.transform, "Crater Exploration", (Type)System.Enum.Parse(typeof(Type), "regular"));
+        mission2Object.transform.Find("Icons/Letter/LetterText").GetComponent<TextMeshPro>().text = newMission2.GetLetter();
+        createRegularButton(newMission2.GetTitle(), newMission2.GetLetter());
+        createRoverButtons(newMission2.GetTitle(), newMission2.GetLetter());
+        missionList.Add(newMission2);
+        roverList.Add(newMission2);
+        allWaypoints.Add(newMission2);
+
+        Waypoint newMission3 = new Waypoint(mission3Object.transform, "Lunar Mapping", (Type)System.Enum.Parse(typeof(Type), "regular"));
+        mission3Object.transform.Find("Icons/Letter/LetterText").GetComponent<TextMeshPro>().text = newMission3.GetLetter();
+        createRegularButton(newMission3.GetTitle(), newMission3.GetLetter());
+        createRoverButtons(newMission3.GetTitle(), newMission3.GetLetter());
+        missionList.Add(newMission3);
+        roverList.Add(newMission3);
+        allWaypoints.Add(newMission3);
+    }
 
     public void OpenGeoConfirmationScreenTest()
     {
         string type = "geosample"; // Test type
 
-        string title = "Test geo title"; // Test title
 
         // CALLS THE FUNCTION TO MAKE
-        OpenConfirmationScreen(type, title);
+        OpenConfirmationScreen(type);
     }
 
     public void OpenRegConfirmationScreenTest()
     {
         string type = "regular"; // Test type
 
-        string title = "Test regular title"; // Test title
 
         // CALLS THE FUNCTION TO MAKE
-        OpenConfirmationScreen(type, title);
+        OpenConfirmationScreen(type);
     }
 
     public void OpenDangerConfirmationScreenTest()
     {
         string type = "danger"; // Test type
 
-        string title = "Test regular title"; // Test title
 
         // CALLS THE FUNCTION TO MAKE
-        OpenConfirmationScreen(type, title);
+        OpenConfirmationScreen(type);
     }
 
-    void OpenConfirmationScreen(string type, string title)
+    void OpenConfirmationScreen(string type)
     {
+
         // Add the type (Tag look at figma for the different tags. There can be "geosample, danger, regular"
         // Add the title of the waypoint in text given the title parameter
 
         TextMeshPro titleText = waypointConfirmationScreen.transform.Find("Text/TitleText").GetComponent<TextMeshPro>();
-        titleText.text = title;
+        titleText.text = "Set title with VEGA";
 
 
         waypointConfirmationScreen.SetActive(true);
@@ -308,21 +369,21 @@ public class NavScreenController : MonoBehaviour
                 missionTag.SetActive(false);
                 obstacleTag.SetActive(true);
                 globalWaypointType = type;
-                globalWaypointTextTitle = title;
+                globalWaypointTextTitle = "Danger";
                 break;
             case "geosample":
                 missionTag.SetActive(false);
                 obstacleTag.SetActive(false);
                 geoTag.SetActive(true);
                 globalWaypointType = type;
-                globalWaypointTextTitle = title;
+                globalWaypointTextTitle = "GeoSample";
                 break;
             case "regular":
                 obstacleTag.SetActive(false);
                 geoTag.SetActive(false);
                 missionTag.SetActive(true);
                 globalWaypointType = type;
-                globalWaypointTextTitle = title;
+                globalWaypointTextTitle = "Mission";
                 break;
             default:
                 Debug.Log("Unknown waypoint type");
@@ -331,8 +392,26 @@ public class NavScreenController : MonoBehaviour
 
     }
 
+    public void SetWaypointTitle(string title)
+    {
+        globalWaypointTextTitle = title;
+        TextMeshPro titleText = waypointConfirmationScreen.transform.Find("Text/TitleText").GetComponent<TextMeshPro>();
+        titleText.text = title;
+
+    }
+
+    public void SetWaypointLat(string lat)
+    {
+        globalLat = lat;
+    }
+
+    public void SetWaypointLong(string longt)
+    {
+        globalLong = longt;
+    }
+
     // THIS FUNCTION IS CALLED BY VEGA
-    public void OpenWaypoint(string type, string title)
+    public void OpenWaypoint(string type)
     {
         // ERROR HANDLING ON IF THE TYPE IS NOT 1 of the 3 WAYPOINT TYPES
         switch (type)
@@ -348,7 +427,7 @@ public class NavScreenController : MonoBehaviour
                 break;
         }
 
-        OpenConfirmationScreen(type, title);
+        OpenConfirmationScreen(type);
     }
 
 
@@ -386,39 +465,15 @@ public class NavScreenController : MonoBehaviour
         {
             case "geosample":
                 count++;
-                // Gets the position of the GeoButtons parent gameobject
-                // This is where the buttons will be instaniated
-                Transform geoButtons = geoScreen.transform.Find("GeoButtons");
-                position = geoButtons.position;
+                createGeoButton(title, letter);
 
-                // Find the yOffset so that the new buttons are below each other
-                yOffset = -0.04f * geoList.Count;
-                position.y += yOffset;
-
-                // Create the buttons and set their title and letter
-                GameObject newGeoPrefab = Instantiate(UIWaypoint, position, Quaternion.identity, geoButtons);
-                newGeoPrefab.transform.Find("Text/Title").GetComponent<TextMeshPro>().text = title;
-                newGeoPrefab.transform.Find("Text/Letter").GetComponent<TextMeshPro>().text = letter;
-
-                geoList.Add(newGeoPrefab);
+                geoList.Add(newWaypoint);
                 break;
             case "regular":
                 count++;
-                // Gets the position of the GeoButtons parent gameobject
-                // This is where the buttons will be instaniated
-                Transform missionButtons = missionScreen.transform.Find("MissionButtons");
-                position = missionButtons.position;
+                createRegularButton(title, letter);
 
-                // Find the yOffset so that the new buttons are below each other
-                yOffset = -0.04f * missionList.Count;
-                position.y += yOffset;
-
-                // Create the buttons and set their title and letter
-                GameObject newMissionPrefab = Instantiate(UIWaypoint, position, Quaternion.identity, missionButtons);
-                newMissionPrefab.transform.Find("Text/Title").GetComponent<TextMeshPro>().text = title;
-                newMissionPrefab.transform.Find("Text/Letter").GetComponent<TextMeshPro>().text = letter;
-
-                missionList.Add(newMissionPrefab);
+                missionList.Add(newWaypoint);
                 break;
             default:
                 Debug.Log("Unknown waypoint type");
@@ -428,20 +483,61 @@ public class NavScreenController : MonoBehaviour
         if (count > 0)
         {
             // The same as above but every waypoint is added to the rover buttons
-            Transform roverButtons = roverScreen.transform.Find("RoverButtons");
-            Vector3 roverPositionUI = roverButtons.position;
-            float roveryOffset = -0.04f * roverList.Count;
-            roverPositionUI.y += roveryOffset;
-            GameObject newRoverUIPrefab = Instantiate(UIWaypoint, roverPositionUI, Quaternion.identity, roverButtons);
-            newRoverUIPrefab.transform.Find("Text/Title").GetComponent<TextMeshPro>().text = title;
-            newRoverUIPrefab.transform.Find("Text/Letter").GetComponent<TextMeshPro>().text = letter;
+            createRoverButtons(title, letter);
 
-            roverList.Add(newRoverUIPrefab);
+            roverList.Add(newWaypoint);
         }
  
 
 
     }
+
+    private void createRoverButtons(string title, string letter)
+    {
+        Transform roverButtons = roverScreen.transform.Find("RoverButtons");
+        Vector3 roverPositionUI = roverButtons.position;
+        float roveryOffset = -0.04f * roverList.Count;
+        roverPositionUI.y += roveryOffset;
+        GameObject newRoverUIPrefab = Instantiate(UIWaypoint, roverPositionUI, Quaternion.identity, roverButtons);
+        newRoverUIPrefab.transform.Find("Text/Title").GetComponent<TextMeshPro>().text = title;
+        newRoverUIPrefab.transform.Find("Text/Letter").GetComponent<TextMeshPro>().text = letter;
+    }
+
+    private void createGeoButton(string title, string letter)
+    {
+        // Gets the position of the GeoButtons parent gameobject
+        // This is where the buttons will be instaniated
+        Transform geoButtons = geoScreen.transform.Find("GeoButtons");
+        Vector3 position = geoButtons.position;
+
+        // Find the yOffset so that the new buttons are below each other
+        float yOffset = -0.04f * geoList.Count;
+        position.y += yOffset;
+
+        // Create the buttons and set their title and letter
+        GameObject newGeoPrefab = Instantiate(UIWaypoint, position, Quaternion.identity, geoButtons);
+        newGeoPrefab.transform.Find("Text/Title").GetComponent<TextMeshPro>().text = title;
+        newGeoPrefab.transform.Find("Text/Letter").GetComponent<TextMeshPro>().text = letter;
+
+    }
+
+    private void createRegularButton(string title, string letter)
+    {
+        // Gets the position of the GeoButtons parent gameobject
+        // This is where the buttons will be instaniated
+        Transform missionButtons = missionScreen.transform.Find("MissionButtons");
+        Vector3 position = missionButtons.position;
+
+        // Find the yOffset so that the new buttons are below each other
+        float yOffset = -0.04f * missionList.Count;
+        position.y += yOffset;
+
+        // Create the buttons and set their title and letter
+        GameObject newMissionPrefab = Instantiate(UIWaypoint, position, Quaternion.identity, missionButtons);
+        newMissionPrefab.transform.Find("Text/Title").GetComponent<TextMeshPro>().text = title;
+        newMissionPrefab.transform.Find("Text/Letter").GetComponent<TextMeshPro>().text = letter;
+    }
+
 
     public void CrewTestNav()
     {
@@ -456,6 +552,34 @@ public class NavScreenController : MonoBehaviour
     public void updateCurrentEnd(Transform end)
     {
         currentEndPosition = end;
+    }
+
+    public void SelectWaypointLetter()
+    {
+        switch (currentScreenOpen)
+        {
+            case "Menu":
+
+                break;
+            case "Crew":
+
+                break;
+            case "Geo":
+
+                break;
+            case "Mission":
+
+                break;
+            case "Rover":
+
+                break;
+            case "Lander":
+
+                break;
+            default:
+                Debug.Log("No Screen Open For Navigation");
+                break;
+        }
     }
 
     public void StartNav()
