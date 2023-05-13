67a8db52379279b1bd8372bdeffe1299947f3b8d
pnhalim <pnhalim@umich.edu>
2023-05-13 01:49:54 -0400

    add UIA egress flow (in progress)

--- Assets/FakeTSSMessageSender.cs
+++ Assets/FakeTSSMessageSender.cs
@@ -6,11 +6,21 @@ using TSS.Msgs;
 public class FakeTSSMessageSender : MonoBehaviour
 {
     public UIAMsg fakeUIA = new UIAMsg();
+    public UIAControlMsg fakeUIAControl = new UIAControlMsg();
 
-
-    public void Fake_SetUIAMsg()
+    private void Start()
     {
-        Simulation.User.UIA = fakeUIA;
-        EventBus.Publish<UIAMsgEvent>(new UIAMsgEvent());
+        StartCoroutine(Fake_SetUIA());
+    }   
+
+    IEnumerator Fake_SetUIA()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1f);
+            Simulation.User.UIA = fakeUIA;
+            Simulation.User.UIA_CONTROLS = fakeUIAControl;
+            EventBus.Publish<UIAMsgEvent>(new UIAMsgEvent());
+        }
     }
 }
