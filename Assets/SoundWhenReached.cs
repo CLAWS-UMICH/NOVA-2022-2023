b1d39cbf28db7b16928641116ffa56cdfde8332f
aprilyc <aprilyc@umich.edu>
2023-05-17 10:39:25 -0400

    sound when waypoint reached

---
+++ Assets/SoundWhenReached.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundWhenReached : MonoBehaviour
+{
+    GameObject waypoint;
+    GameObject player;
+    private bool played = false;
+    private float radius = 2f;
+    private float distance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Main Camera");
+        waypoint = GameObject.Find("WaypointSign");
+        distance = Vector3.Distance(waypoint.transform.position, player.transform.position);
+        played = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance = Vector3.Distance(waypoint.transform.position, player.transform.position);
+        if (!played && distance <= radius)
+        {
+            GetComponent<AudioSource>().Play();
+            played = true;
+        }
+    }
+}
