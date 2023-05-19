7fc7dc39d41aba0cb1c036614e4b175a6a6a18ac
pnhalim <pnhalim@umich.edu>
2023-05-19 16:56:31 -0400

    fix tss??

--- Assets/CLAWS/Overall System/Backend/TelemetryServerManager.cs
+++ Assets/CLAWS/Overall System/Backend/TelemetryServerManager.cs
@@ -30,7 +30,7 @@ public class TelemetryServerManager : MonoBehaviour
         string team_name = "CLAWS";
         string username = "Patrick";
         string university = "University of Michigan";
-        string user_guid = "fdbee7e5-9887-495e-aabb-f10d1386a7e9";
+        string user_guid = "eb0dde22-a403-45cd-a3bc-45c797634d32";
         tssUri = "ws://192.168.50.10:3001"; // TODO fix
         var connecting = tss.ConnectToURI(tssUri, team_name, username, university, user_guid);
         Debug.Log("Connecting to " + tssUri);
