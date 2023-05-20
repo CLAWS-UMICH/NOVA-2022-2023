dabeadebafce0e0a5496a39c271dc11750d62052
Kriti Gupta <kritigupta@gmail.com>
2023-05-20 18:43:35 -0400

    close messaging event

--- Assets/CLAWS/Messaging/MessagingNewHandler.cs
+++ Assets/CLAWS/Messaging/MessagingNewHandler.cs
@@ -22,11 +22,12 @@ public class MessagingNewHandler : MonoBehaviour
 
     void Start()
     {
+        EventBus.Subscribe<CloseEvent>(CloseMessaging);
         reply = FiveMinuteReply(true);
         StartCoroutine(reply);
         
     }
-    public void CloseMessaging(){
+    public void CloseMessaging(CloseEvent e){
         messaging.SetActive(false);
     }
 
