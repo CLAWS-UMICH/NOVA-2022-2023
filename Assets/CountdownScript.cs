6e92cb60e65a46eee957316e457f2579aa8ee4d6
adhav <uniqname@umich.edu>
2023-05-20 23:35:11 -0500

    added countdown

---
+++ Assets/CountdownScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+[System.Serializable]
+public class CountdownScript : MonoBehaviour
+{
+    [SerializeField]
+    TextMeshPro Number;
+    [SerializeField]
+    int CountdownNumber;
+
+    void Awake()
+    {
+        StartCoroutine(Countdown());
+        Number.text = CountdownNumber.ToString();
+    }
+
+    IEnumerator Countdown() {
+        if(CountdownNumber < 1) {
+            gameObject.SetActive(false);
+        }
+        yield return new WaitForSeconds(1f);
+        CountdownNumber--;
+        Number.text = CountdownNumber.ToString();
+    }
+}
