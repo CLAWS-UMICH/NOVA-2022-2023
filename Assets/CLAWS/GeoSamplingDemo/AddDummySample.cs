5d249d3bfc3a54b3399202bfa902423bd169fecf
pnhalim <pnhalim@umich.edu>
2023-05-22 02:06:47 -0400

    asdfgbnhfdasfgb

--- Assets/CLAWS/GeoSamplingDemo/AddDummySample.cs
+++ Assets/CLAWS/GeoSamplingDemo/AddDummySample.cs
@@ -19,6 +19,12 @@ public class AddDummySample : MonoBehaviour
 
     [ContextMenu("AddRandomSample")]
     public void AddRandomSample() {
+        StartCoroutine(_AddRandomSample());
+    }
+
+    IEnumerator _AddRandomSample()
+    {
+        yield return new WaitForSeconds(3f);
         Simulation.User.AstronautGeoSamples.geoSampleList.Insert(0, new GeoSample(id, rocks[id % 4], System.DateTime.Now.ToString(), "26.234 N, 42.2345 W", (1203948329 * 1.02 * id).ToString(), 'n', "This is a sample description for task " + id.ToString(), spec));
         EventBus.Publish<GeoSampleUpdatedEvent>(new GeoSampleUpdatedEvent(0));
         id++;
